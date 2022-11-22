01484d44c3307a647b6a4ba727721caa9e5709c7
Ney2 <kidistdinku5057@gmail.com>
2022-11-22 18:47:55 +0300

    Fix linter errors

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
-    has_many :entities, dependent: :destroy
+  has_many :entities, dependent: :destroy
 
-    validates :name, presence: true, uniqueness: true
-    validates :icon, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :icon, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :groups, dependent: :destroy
+  has_many :groups, dependent: :destroy
   has_many :entities, dependent: :destroy
   has_one_attached :avatar, dependent: :destroy
   validates :name, presence: true, uniqueness: true
