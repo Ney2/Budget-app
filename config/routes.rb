c78ecfbf56fe6c8e84af7ab9168efd1a43780129
Ney2 <kidistdinku5057@gmail.com>
2022-11-25 02:51:26 +0300

    Update routes of the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'splash/index'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'groups#index', as: :authenticated_root
+    end
+    unauthenticated { root 'splash#index', as: :unauthenticated_root }
+  end
+
+  devise_for :users, controllers: { registrations: 'registrations' }
+
+  resources :splash, only:[:index]
+  resources :users, only: [:show, :income, :update, :destroy]
+  resources :groups, only: [:index ,:show, :new, :create,  :destroy] do
+    resources :entities, only: [:index, :show, :new, :create, :destroy]
+  end
 end
